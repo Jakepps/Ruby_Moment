7bcd1b0e9ff69c5f3cd67f5f92545ab22365cf18
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-03-03 22:47:13 +0300

    жетский рефакторинг + readtxt

--- LR2/student.rb
+++ LR2/student.rb
@@ -1,61 +1,27 @@
 class Student
-  attr_accessor :id, :last_name, :name, :surname, :phone, :telegram, :email, :git
+  attr_accessor :id, :last_name, :name, :surname, :contacts
   
-  def initialize(id:, last_name:, name:, surname: nil, phone: nil, telegram: nil, email: nil, git: nil)
+  def initialize(id:, last_name:, name:, surname: nil, contacts: {})
     @id = id
     @last_name = last_name
     @name = name
     @surname = surname
-    @phone = phone
-    @telegram = telegram
-    @email = email
-    @git = git
+    @contacts = contacts
+    validate
   end
 
   def to_s
     "Студент №#{id}: #{last_name} #{name} #{surname}.\n Телефон: #{phone}, Телеграм: #{telegram}, Почта: #{email}, Git: #{git}"
   end
 
-  def self.from_string(str)
-    # Регулярное выражение для парсинга строки вида "Фамилияб, инициалы ,гит, контакты"
-    regex = /^(\S+)\s+(\S\.\S\.)\s+(\S+),\s+(.*)$/
-    match = regex.match(str)
-    
-    raise ArgumentError, "Неверный формат строки: #{str}" unless match
-    
-    last_name = match[1]
-    initials = match[2]
-    github = match[3]
-    contact = match[4]
-    
-    # Проверка валидности данных
-    validate(last_name, initials, github, contact)
-    
-    # Вызов стандартного конструктора с полученными параметрами
-    self.new(last_name, initials, github, contact)
+  def self.from_string(string)
+    id, last_name, first_name, surname, phone, telegram, email, git = string.split(',')
+    contacts = { phone: phone, telegram: telegram, mail: email, git: git }
+    new(id: id, last_name: last_name, first_name: first_name, surname: surname, contacts: contacts)
   end
 
-  def validate
-    unless self.git
-      raise ArgumentError.new("Не заполнено поле Git")
-    end
-    unless self.phone || self.telegram || self.email
-      raise ArgumentError.new("Не заполнено ни одно поле для связи")
-    end
-  end
-
-  def self.valid_phone?(phone)
-    phone.to_s.gsub(/\D/, '').match?(/^((\+7|7|8)+([0-9]){10})$/)
-  end
-
-  def self.validate(last_name, initials, github, contact)
-    raise ArgumentError, "Неверная фамилия: #{last_name}" unless last_name =~ /^\p{L}+$/
-    
-    raise ArgumentError, "Неверные инициалы: #{initials}" unless initials =~ /^[А-Я]\.[А-Я]\.$/
-    
-    raise ArgumentError, "Неверный GitHub: #{github}" unless github =~ /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i
-    
-    raise ArgumentError, "Неверные контактные данные: #{contact}" unless contact =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  def initials
+    "#{@first_name[0]}.#{@surname[0]}." if @first_name && @surname
   end
 
   def set_contacts(phone: nil, telegram: nil, email: nil)
@@ -66,6 +32,53 @@ class Student
 
   def getInfo
     "#{last_name} #{initials}; GitHub: #{github}, Email: #{contact}"
+  end
+
+  def self.read_from_txt(file_path)
+    students = []
+    begin
+      File.foreach(file_path) do |line|
+        students << from_string(line.strip)
+      end
+      students
+    rescue => exception
+      raise "Файл не найден по указанному адресу #{file_path}. Исключение: #{exception.message}"
+    end
+  end
+  
+  def validate
+    unless self.git
+      raise ArgumentError.new("Не заполнено поле Git")
+    end
+    unless self.phone || self.telegram || self.email
+      raise ArgumentError.new("Не заполнено ни одно поле для связи")
+    end
+  end
+
+  def self.validate(last_name, initials, github, contact)
+    raise ArgumentError, "Неверная фамилия: #{last_name}" unless valid_lastname?
+    
+    raise ArgumentError, "Неверные инициалы: #{initials}" unless valid_initials?
+    
+    raise ArgumentError, "Неверный GitHub: #{github}" unless valid_github?
+    
+    raise ArgumentError, "Неверные контактные данные: #{contact}" unless valid_contact?
+  end
+
+  def valid_lastname?
+    contacts[:last_name].to_s.match?(/^\p{L}+$/)
+  end
+
+  def valid_initials?
+    contacts[:initials].to_s.match?(/^[А-Я]\.[А-Я]\.$/)
+  end
+
+  def valid_github?
+    contacts[:github].to_s.match?(/\Ahttps?:\/\/github\.com\/[a-zA-Z0-9]+\/[a-zA-Z0-9]+\z/)
+  end
+
+  def valid_contact?
+    contacts[:contact].to_s.match?(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
   end
 
 end
