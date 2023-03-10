9c81fef48f4489c86728c9405d8f2972b1881074
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-03-10 15:12:58 +0300

    да, вот комит где я убрал решетку

--- LR2/Task 1-2/main.rb
+++ LR2/Task 1-2/main.rb
@@ -1,13 +1,13 @@
 require_relative 'student'
 
 student1 = Student.new(id: 1, last_name: "Хахук", name: "Рустам", phone: "9(999)999-99-99", telegram: "delta_null", git: "https://github.com/delta_null")
-#puts student1
+puts student1
 
 student2 = Student.new(id: 2, last_name: "Нагалевский", name: "Артем", surname: "Михайлович", email: "nagalevskiy@mail.ru", git: "https://github.com/jakepps")
-#puts student2
+puts student2
 
 student3 = Student.new(id: 3, last_name: "Волков", name: "Серега", telegram: "starproxima", git: "https://github.com/starproxima")
-#puts student3
+puts student3
 
 begin
     students = Student.read_from_txt('test.txt')
