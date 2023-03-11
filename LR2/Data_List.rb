9ffc5443cd33b0ea7a10ea411cd6ce49fd20b78a
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-03-11 09:59:37 +0300

    Update Data_List.rb

--- LR2/Data_List.rb
+++ LR2/Data_List.rb
@@ -1,49 +1,54 @@
 class Data_list
+  attr_reader :selected
 
-    def initialize(*elements)
-      @data = elements.sort
-      @frozen = true
-      @selected = []
-    end
-  
-    def [](index)
-      @data[index]
-    end
-  
-    def each(&block)
-      @data.each(&block)
-    end
-  
-    def size
-      @data.size
-    end
-  
-    def reverse!
-      raise "Data_list заморожен" if @frozen
-      @data.reverse!
-    end
-  
-    def self.create(*elements)
-      new(*elements)
-    end
+  def initialize(*elements)
+    @data = elements.sort
+    @frozen = true
+    @selected = []
+  end
 
-    def select(number)
-        @selected << number
-    end
+  def [](index)
+    @data[index]
+  end
 
-    def get_selected
-        @selected
-    end
+  def each(&block)
+    @data.each(&block)
+  end
 
-    def get_names
-        raise NotImplementedError, "Должен быть реализован в подклассе"
-    end
+  def size
+    @data.size
+  end
 
-    def get_data
-        raise NotImplementedError, "Должен быть реализован в подклассе"
-    end
-    
-    def each
-        @data.each { |element| yield element }
-      end
+  def reverse!
+    raise "Data_list заморожен" if @frozen
+    @data.reverse!
+  end
+
+  def self.create(*elements)
+    new(*elements)
+  end
+
+  def select(number)
+      @selected << number
+  end
+
+  def get_selected
+      @selected
+  end
+
+  def get_names
+      raise NotImplementedError, "Должен быть реализован в подклассе"
+  end
+
+  def get_data
+      raise NotImplementedError, "Должен быть реализован в подклассе"
+  end
+  
+  def each
+      @data.each { |element| yield element }
+  end
+
+  private
+
+  attr_reader :data
 end
