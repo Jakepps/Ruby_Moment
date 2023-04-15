93817f37e12c79c685a64193ae7f7a60b385c309
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-04-15 09:21:07 +0300

    переделал

---
+++ LR3/Tab_students.rb
@@ -0,0 +1,74 @@
+
+class TabStudents
+    include Glimmer::LibUI::CustomControl
+  
+    body {
+      horizontal_box {
+        # Секция 1
+        vertical_box {
+          stretchy false
+  
+          form {
+            stretchy false
+  
+            @filter_last_name_initials = entry {
+              label 'Фамилия И. О.'
+            }
+  
+            @filters = {}
+            fields = [[:git, 'Github'], [:email, 'Почта'], [:phone, 'Телефон'], [:telegram, 'Телеграм']]
+  
+            fields.each do |field|
+              @filters[field[0]] = {}
+  
+              @filters[field[0]][:combobox] = combobox {
+                label "#{field[1]} имеется?"
+                items ['Не важно', 'Есть', 'Нет']
+                selected 0
+  
+                on_selected do
+                  if @filters[field[0]][:combobox].selected == 1
+                    @filters[field[0]][:entry].read_only = false
+                  else
+                    @filters[field[0]][:entry].text = ''
+                    @filters[field[0]][:entry].read_only = true
+                  end
+                end
+              }
+  
+              @filters[field[0]][:entry] = entry {
+                label field[1]
+                read_only true
+              }
+            end
+          }
+        }
+  
+        # Секция 2
+        vertical_box {
+          @table = table {
+  
+            text_column('ФИО')
+            text_column('Github')
+            text_column('Контакт')
+  
+            editable false
+  
+            cell_rows [['Нагалевский А.М.', 'jakepps', '+79995557722'], ['Хахук Р.Н.', 'delta_null', '+79183939494'], ['Гиренко Д.Е.', nil, 'gir@mail.ru'], ['Прозоров М.С.', nil, nil]]
+          }
+  
+          @pages = horizontal_box {
+            stretchy false
+  
+            button('1')
+            button('2')
+            button('3')
+            label('...') { stretchy false }
+            button('20')
+          }
+        }
+  
+ 
+      }
+    }
+  end
--- LR3/test.rb
+++ LR3/test.rb
@@ -1,34 +1,20 @@
-require 'fox16'
+require 'glimmer-dsl-libui'
+require_relative 'Tab_students'
 
-include Fox
-
-class ListWindow < FXMainWindow
-  def initialize(app)
-    super(app, "Пример", :width => 400, :height => 200)
-
-    @list_widget = FXList.new(self, nil, 0, LIST_SINGLESELECT|LAYOUT_FILL_X|LAYOUT_FILL_Y)
-    @list_widget.appendItem("Список 1")
-    @list_widget.appendItem("Список 2")
-    @list_widget.appendItem("Список 3")
-
-    @list_widget.connect(SEL_COMMAND) do |sender, selector, data|
-      if sender == @list_widget
-        selected_item = @list_widget.getItemText(@list_widget.currentItem)
-        puts "Выберите список: #{selected_item}"
-      end
-    end
-
-    close_button = FXButton.new(self, "Close", :opts => FRAME_RAISED|FRAME_THICK|LAYOUT_RIGHT|LAYOUT_TOP, :width => 100, :height => 30)
-    close_button.connect(SEL_COMMAND) do
-      self.close
-    end
-
-    # Вызываем метод show для отображения окна
-    show(PLACEMENT_SCREEN)
+class MainWindow
+  include Glimmer
+  def create
+    window('Универ', 600, 200) {
+      tab {
+        tab_item('Студенты') {
+          tab_students
+        }
+        tab_item('Вкладка 2') { }
+        tab_item('Вкладка 3') { }
+      }
+    }
   end
 end
 
-app = FXApp.new
-ListWindow.new(app)
-app.create
-app.run
+main_window = MainWindow.new.create
+main_window.show
