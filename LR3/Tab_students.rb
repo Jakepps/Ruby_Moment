f0737d1696968ae06c1b910117cdd78f6a0266e2
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-04-15 09:51:19 +0300

    добавлены 3 секции

--- LR3/Tab_students.rb
+++ LR3/Tab_students.rb
@@ -12,7 +12,7 @@ class TabStudents
             stretchy false
   
             @filter_last_name_initials = entry {
-              label 'Фамилия И. О.'
+              label 'ФИО'
             }
   
             @filters = {}
@@ -68,7 +68,15 @@ class TabStudents
           }
         }
   
- 
+        # Секция 3
+        vertical_box {
+          stretchy true
+  
+          button('Добавить') { stretchy false }
+          button('Изменить') { stretchy false }
+          button('Удалить') { stretchy false }
+          button('Обновить') { stretchy false }
+        }
       }
     }
   end
